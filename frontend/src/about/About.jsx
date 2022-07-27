01a11958365265cc700c88a7e717d582a64b222a
MatheusSimoes13 <matheus.ruff@hotmail.com>
2022-07-27 09:33:52 -0300

    Componente menu, configurando as rotas, componente pageheader

--- frontend/src/about/About.jsx
+++ frontend/src/about/About.jsx
@@ -1,8 +1,14 @@
 import React from "react"
+import PageHeader from "../template/PageHeader"
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa história</h2>
+        <p>Teste</p>
+        <h2>Missão</h2>
+        <p>Testeeeeeeeeeeeeee</p>
     </div>
 
 
