26c2f4707f914bc3f2774e19119914fa8dc52412
MatheusSimoes13 <matheus.ruff@hotmail.com>
2022-07-28 11:47:42 -0300

    Redux

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,14 @@
 import React from "react"
 import { render } from "react-dom"
-import App from './main/app'
+import { createStore } from "redux"
+import { Provider } from "react-redux"
 
-render(<App />, document.getElementById('app'))
+import App from './main/app'
+import reducers from './main/reducers'
+
+const store = createStore(reducers)
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+,document.getElementById('app'))
